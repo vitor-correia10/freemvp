d800fc44157377ccd9ac7d16da04982922871c79
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2020-10-07 19:21:39 -0400

    initial setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 8080;
+
+express()
+    .use(morgan("tiny"))
+    .use(express.static("public"))
+    .use(bodyParser.json())
+    .use(express.urlencoded({ extended: false }))
+    .use("/", express.static(__dirname + "/"))
+
+
+
+    // handle 404s
+    .use((req, res) => res.status(404).type("txt").send("Error 404"))
+
+    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
